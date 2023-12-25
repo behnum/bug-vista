1ece21fad8388504c23af0a8387e1f5b6372a620
Behnam Baharmand <bbaharmand@gmail.com>
2023-12-25 11:05:59 +0100

    Add client-side rendering to NavBar and create IssuesPage component

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { GoIssueTracks } from "react-icons/go";
+import classNames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { href: "/", label: "Dashboard" },
     { href: "/issues", label: "Issues" },
   ];
+
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
@@ -15,7 +22,11 @@ const NavBar = () => {
         {links.map((link) => (
           <Link
             key={link.href}
-            className="text-emerald-500 hover:text-emerald-800 transition-colors"
+            className={classNames({
+              "text-emerald-800": currentPath === link.href,
+              "text-emerald-500": currentPath !== link.href,
+              "hover:text-emerald-800 transition-colord": true,
+            })}
             href={link.href}
           >
             {link.label}
