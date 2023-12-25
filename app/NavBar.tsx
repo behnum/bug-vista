d8e825cfdfaff3a927639edb1c74491e176c15ce
Behnam Baharmand <bbaharmand@gmail.com>
2023-12-25 10:51:28 +0100

    Refactor NavBar component to use dynamic links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -2,18 +2,25 @@ import Link from "next/link";
 import { GoIssueTracks } from "react-icons/go";
 
 const NavBar = () => {
+  const links = [
+    { href: "/", label: "Dashboard" },
+    { href: "/issues", label: "Issues" },
+  ];
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
         <GoIssueTracks />
       </Link>
       <ul className="flex space-x-6">
-        <li>
-          <Link href="/">Dashboard</Link>
-        </li>
-        <li>
-          <Link href="/issues">Issues</Link>
-        </li>
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className="text-emerald-500 hover:text-emerald-800 transition-colors"
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
