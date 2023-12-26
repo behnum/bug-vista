71012294aadb6472a972c4bbcbb17b6bf85fa974
Behnam Baharmand <bbaharmand@gmail.com>
2023-12-26 10:00:24 +0100

    Add @radix-ui/themes package and use Button component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+import '@radix-ui/themes/styles.css'
 import './globals.css'
 import type { Metadata } from 'next'
+import { Theme } from '@radix-ui/themes'
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
 
@@ -14,8 +16,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <NavBar />
-        <main>{children}</main>
+        <Theme>
+          <NavBar />
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   )
