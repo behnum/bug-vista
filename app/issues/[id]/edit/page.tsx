08f037f43a2396e262dbc33ff910c54caa5201b4
Behnam Baharmand <bbaharmand@gmail.com>
2023-12-27 15:25:05 +0100

    Add EditIssuePage and IssueForm components

---
+++ app/issues/[id]/edit/page.tsx
@@ -0,0 +1,24 @@
+import { notFound } from 'next/navigation'
+import IssueForm from '../../_components/IssueForm'
+import prisma from '@/prisma/client'
+
+interface Props {
+  params: { id: string }
+}
+
+const EditIssuePage = async ({ params }: Props) => {
+  if (isNaN(parseInt(params.id))) {
+    notFound()
+  }
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) }
+  })
+
+  if (!issue) {
+    notFound()
+  }
+
+  return <IssueForm issue={issue} />
+}
+export default EditIssuePage
