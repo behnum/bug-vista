96be293236a6b3823edd82b74e0e054e9dfe351c
Behnam Baharmand <bbaharmand@gmail.com>
2023-12-27 09:22:23 +0100

    Add react-markdown and @tailwindcss/typography dependencies (add MD preview)

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,6 +3,7 @@ import prisma from '@/prisma/client'
 import { Card, Flex, Heading, Text } from '@radix-ui/themes'
 import delay from 'delay'
 import { notFound } from 'next/navigation'
+import ReactMarkdown from 'react-markdown'
 
 interface Props {
   params: { id: string }
@@ -30,8 +31,8 @@ const IssueDetailPage = async ({ params }: Props) => {
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card>
-        <p>{issue.description}</p>
+      <Card className='prose' mt='6'>
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
     </div>
   )
