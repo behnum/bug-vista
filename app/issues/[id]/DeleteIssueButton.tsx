5dddbc65398c1a4969a9a51d387b6fd30f75b445
Behnam Baharmand <bbaharmand@gmail.com>
2023-12-28 09:39:46 +0100

    Add a confirmation dialog box Refactor delete issue button and add variant to edit issue button

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,11 +1,31 @@
-import { Button } from '@radix-ui/themes'
+'use client'
+
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import { RiDeleteBinLine } from 'react-icons/ri'
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color='red'>
-      <RiDeleteBinLine /> Delete
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button variant='classic' color='red'>
+          <RiDeleteBinLine /> Delete
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Are you sure?</AlertDialog.Title>
+        <AlertDialog.Description>This action cannot be undone.</AlertDialog.Description>
+        <Flex gap='3' mt='4'>
+          <AlertDialog.Cancel>
+            <Button color='gray'>Cancel</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant='classic' color='red'>
+              Delete
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   )
 }
 
