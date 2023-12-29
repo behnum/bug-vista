3167045c4f47025bfc2908997466779f44239719
Behnam Baharmand <bbaharmand@gmail.com>
2023-12-29 10:31:00 +0100

    Add QueryClientProvider component and import React Query

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import NavBar from './NavBar'
 import './globals.css'
 import './theme-config.css'
 import AuthProvider from './auth/Provider'
+import QueryClientProvider from './QueryClientProvider'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -21,14 +22,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body className={inter.variable}>
-        <AuthProvider>
-          <Theme accentColor='grass' radius='large'>
-            <NavBar />
-            <main className='p-5'>
-              <Container>{children}</Container>
-            </main>
-          </Theme>
-        </AuthProvider>
+        <QueryClientProvider>
+          <AuthProvider>
+            <Theme accentColor='grass' radius='large'>
+              <NavBar />
+              <main className='p-5'>
+                <Container>{children}</Container>
+              </main>
+            </Theme>
+          </AuthProvider>
+        </QueryClientProvider>
       </body>
     </html>
   )
