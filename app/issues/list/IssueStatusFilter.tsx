097d4d5effd91ba5833405056861506356c04be1
Behnam Baharmand <bbaharmand@gmail.com>
2023-12-30 07:03:51 +0100

    Resolve bugs in filtering

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -15,6 +15,7 @@ const IssueStatusFilter = () => {
 
   return (
     <Select.Root
+      defaultValue={searchParams.get('status') || ''}
       onValueChange={status => {
         const params = new URLSearchParams()
         if (status) params.append('status', status)
